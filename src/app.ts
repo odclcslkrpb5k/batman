013d47a46a066afef5c65dd0644998fa8a35f943
Robert Bullock <robert.bullock@gmail.com>
2023-11-20 22:14:29 -0500

    initial stuff

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+// ESM
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: true
+});
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' });
+});
+
+const defaultPort = '5000';
+const { PORT } = process.env;
+const port: number = parseInt((PORT) ? PORT : defaultPort);
+
+import { Pool } from "pg";
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
+
+
+// Run the server!
+fastify.listen({ port, host: '0.0.0.0' }, (err, address) => {
+  if (err) throw err;
+  // Server is now listening on ${address}
+});
