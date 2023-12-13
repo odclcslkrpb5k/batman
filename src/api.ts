1f36a845e3cb4139b20d6c6e74e00053986ab4ba
Robert Bullock <robert.bullock@gmail.com>
2023-12-12 23:19:02 -0500

    Continued work on thing creation and locating

--- src/api.ts
+++ src/api.ts
@@ -8,8 +8,13 @@ interface CreateThingBodyType {
   attributes: string;
 };
 
+interface ThingRequestKeyType {
+  key: string;
+};
+
 type PostThingRequest = FastifyRequest<{
-  Body: CreateThingBodyType
+  Body: CreateThingBodyType,
+  Params: ThingRequestKeyType
 }>;
 
 interface ThingRequestQueryStringType {
@@ -21,51 +26,69 @@ interface ThingRequestQueryStringType {
   offset: number|null;
   context: string|null;
 };
-interface ThingRequestKeyType {
-  key: string;
-};
 type ThingRequest = FastifyRequest<{
   Params: ThingRequestKeyType,
   Querystring: ThingRequestQueryStringType
 }>
 
+const get_thing = async(pool: Pool, key: number) => {
+  const res = await pool.query('select * from thing where key = $1', [key]);
+  return res.rows[0];
+};
+
+const get_things_by_type = async(pool: Pool, type: number) => {
+  const res = await pool.query('select * from thing where type_key = $1', [type]);
+  return res.rows;
+};
+
+const get_root_thing = async(pool: Pool) => {
+  const res = await pool.query('select * from thing join thing_type on (thing.type_key=thing_type.key) where max_locations = 0');
+  return res.rows[0];
+};
+
+const create_thing = async(pool: Pool, name: string, description: string, type: number, attributes: string, location_key: number) => {
+  const res = await pool.query('insert into thing (name, description, type_key, attributes) VALUES ($1, $2, $3, $4) RETURNING *', [name, description, type, attributes]);
+  const newThing = res.rows[0];
+  const res2 = await pool.query('insert into thing_location (thing_key, location_key) VALUES ($1, $2) RETURNING *', [newThing.key, location_key]);
+  return newThing;
+};
+
 export const api = (fastify: FastifyInstance, pool: Pool) => {
-  fastify.get('/thing', (request: ThingRequest, reply) => {
+  fastify.get('/thing', async(request: ThingRequest, reply) => {
     const qs = request.query;
-    reply.send({ qs });
+    // reply.send({ qs });
     // perform GET-THING(root)
+    const root = await get_root_thing(pool);
+    reply.send(root);
   });
 
   fastify.post('/thing', async (request: PostThingRequest, reply) => {
     const body = request.body;
     const { name, description, type, attributes } = body;
-    // reply.send({ });
-    // create a new thing with body params with this thing as the location
-    await new Promise<void>((resolve,reject) => {
-      pool.query('insert into thing (name, description, type_key, attributes) VALUES ($1, $2, $3, $4) RETURNING *', [name, description, type, attributes], (err, res) => {
-        if (err) {
-          console.log(err.stack);
-          reply.status(200).send({ message: 'Error creating thing (2)', error: err.message });
-          reject(err);
-        } else {
-          // console.log(res.rows[0]);
-          reply.status(201).send(res.rows[0]);
-          resolve();
-        }
-      });
-    });
+    // get the root thing first
+    const root = await get_root_thing(pool);
+    // create a new thing with body params with the root thing as the location
+    const newThing = await create_thing(pool, name, description, type, attributes, root.key);
+    reply.status(201).send(newThing);
   });
 
-  fastify.get('/thing/:key', (request: ThingRequest, reply) => {
+  fastify.get('/thing/:key', async(request: ThingRequest, reply) => {
     const key = request.params.key;
     const { limit } = request.query;
-    reply.send({ key });
+    // reply.send({ key });
     // perform GET-THING(key)
+    const thing = await get_thing(pool, parseInt(key));
+    reply.send(thing);
   });
-  fastify.post('/thing/:key', (request: ThingRequest, reply) => {
+  fastify.post('/thing/:key', async (request: PostThingRequest, reply) => {
     const key = request.params.key;
-    reply.send({ key });
+    // reply.send({ key });
+    // get the new thing params from the body
+    const body = request.body;
+    const { name, description, type, attributes } = body;
     // create a new thing with body params with this thing as the location
+    const newThing = await create_thing(pool, name, description, type, attributes, parseInt(key));
+    reply.status(201).send(newThing);
   });
   fastify.put('/thing/:key', (request: ThingRequest, reply) => {
     const key = request.params.key;
